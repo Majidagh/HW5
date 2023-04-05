995c1bcf9d310c45f3aae4083d624f4fc2967f45
Majid Aghighi <majid.aghighy@gmail.com>
2023-04-05 20:21:21 +0330

    New Update

---
+++ script.js
@@ -0,0 +1,31 @@
+// Q #1
+
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+
+// user.sayHello = function () {
+//   alert(`Hello ${user.name}`);
+// };
+
+// user.sayHello();
+
+//--------------------------------------------------------
+//Q #2
+
+//----------------
+// Q #3
+
+// const employees = [];
+// employees[0] = { name: "Majid", age: 32, retiredate: "March 12, 2014" };
+// employees[1] = { name: "Ali", age: 17, retiredate: "June 2, 2023" };
+// employees[2] = { name: "Hamed", age: 58, retiredate: "December 20, 2036" };
+// employees[3] = { name: "Navid", age: 62, retiredate: "April 30, 2020" };
+
+// employees.sort(function (a, b) {
+//   return a.age - b.age;
+// });
+
+// console.log(employees.sort());
+//--------------------------------------------------------
